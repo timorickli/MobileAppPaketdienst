d8317da43b323c11cbf10dc7cdaa325b63b3ed67
Timo Rickli <timo.rickli@ict-campus.net>
2020-05-14 13:09:35 +0200

    Created

--- app/src/main/java/net/ictcampus/paketdienst/MainActivity.java
+++ app/src/main/java/net/ictcampus/paketdienst/MainActivity.java
@@ -2,7 +2,9 @@ package net.ictcampus.paketdienst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent= new Intent(getApplicationContext(), MapActivity.class);
+        startActivity(intent);
     }
 }
