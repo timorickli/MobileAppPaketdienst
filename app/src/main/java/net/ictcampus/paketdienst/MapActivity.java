6802f70b4a2cb880d30f074a386fb95eca801676
Timo Rickli <timo.rickli@ict-campus.net>
2020-05-15 09:56:55 +0200

    Created

--- app/src/main/java/net/ictcampus/paketdienst/MapActivity.java
+++ app/src/main/java/net/ictcampus/paketdienst/MapActivity.java
@@ -2,22 +2,42 @@ package net.ictcampus.paketdienst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends Activity implements OnMapReadyCallback {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        MapFragment mapFragment= (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng position = new LatLng( -33.867, 151.206);
+        googleMap.setMyLocationEnabled(true);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 13));
+
+        googleMap.addMarker(new MarkerOptions()
+                .title("Test")
+                .snippet("Tester")
+                .position(position)
+        );
+    }
 }
