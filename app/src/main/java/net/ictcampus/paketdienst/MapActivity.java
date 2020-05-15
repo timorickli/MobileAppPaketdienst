eff0c9e5cbb1f6ed48f73d2287cbf2e152dfb62f
Timo Rickli <timo.rickli@ict-campus.net>
2020-05-15 09:19:50 +0200

    Created

--- app/src/main/java/net/ictcampus/paketdienst/MapActivity.java
+++ app/src/main/java/net/ictcampus/paketdienst/MapActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MapActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
     }
+
+
 }
