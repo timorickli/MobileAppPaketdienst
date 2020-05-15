a63501884530db215da6ce2092fcddb328860992
Timo Rickli <timo.rickli@ict-campus.net>
2020-05-15 15:52:48 +0200

    Location

--- app/src/main/java/net/ictcampus/paketdienst/MainActivity.java
+++ app/src/main/java/net/ictcampus/paketdienst/MainActivity.java
@@ -1,18 +1,80 @@
 package net.ictcampus.paketdienst;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent= new Intent(getApplicationContext(), MapActivity.class);
-        startActivity(intent);
+        checkPersmissionsLocation();
+
+    }
+    //Testen ob die Berechtigungen für die Location gesetzt wurden
+    private void checkPersmissionsLocation(){
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //wenn sie bereits gesetzt sind (vor API level 24)
+            Intent intent= new Intent(getApplicationContext(), MapActivity.class);
+            startActivity(intent);
+
+        }
+        else {
+            //Keine Berechtigungen auf die Location
+            if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "Location permission is needed to show the current position.",Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+    }
+    private void checkPermissionCamera(){
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            //wenn sie bereits gesetzt sind (vor API level 24)
+            Intent intent= new Intent(getApplicationContext(), MapActivity.class);
+            startActivity(intent);
+
+        }
+        else {
+            //Keine Berechtigungen auf die Location
+            if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                Toast.makeText(this, "Location permission is needed to show the AR vision.",Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA},1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent= new Intent(getApplicationContext(), MapActivity.class);
+                    startActivity(intent);
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Berechtigung fehlgeschlagen", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case 2:
+
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
--- app/src/main/java/net/ictcampus/paketdienst/MapActivity.java
+++ app/src/main/java/net/ictcampus/paketdienst/MapActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -16,22 +20,52 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Locale;
 import java.util.jar.Manifest;
 
-public class MapActivity extends Activity implements OnMapReadyCallback {
-
+public class MapActivity extends Activity implements OnMapReadyCallback  {
+    private Double personLocationLongitude= 10.1;
+    private Double personLocationLatitude= 10.1;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
+        //Create Map
         MapFragment mapFragment= (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.google_map);
         mapFragment.getMapAsync(this);
+        //Location Manager
+        LocationManager locationManager= (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener= new LocationListener(){
+            @Override
+            public void onLocationChanged(Location location) {
+                personLocationLongitude= location.getLongitude();
+                personLocationLatitude= location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+
+        };
+        //Button Click Event
         ImageButton ib = findViewById(R.id.imageButton);
         ib.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,20 +79,17 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng position = new LatLng( -33.867, 151.206);
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            googleMap.setMyLocationEnabled(true);
-        } else {
-            // Show rationale and request permission.
-        }
+        map= googleMap;
+        map.setMyLocationEnabled(true);
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 13));
+    }
+    public void createPakets(){
 
-        googleMap.addMarker(new MarkerOptions()
-                .title("Test")
-                .snippet("Tester")
-                .position(position)
-        );
+    }
+    public void destroyPakets(){
+
+    }
+    public void createMailBox(){
+
     }
 }
