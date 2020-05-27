22fad40b43ab1dc59a2aab89216b2c9d9dec550d
AP1xel <63781471+AP1xel@users.noreply.github.com>
2020-05-27 13:54:13 +0200

    Working Ar-Map-Packages/Map

--- app/src/main/java/net/ictcampus/paketdienst/ArActivity.java
+++ app/src/main/java/net/ictcampus/paketdienst/ArActivity.java
@@ -2,41 +2,31 @@ package net.ictcampus.paketdienst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.filament.View;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 import com.google.ar.core.Pose;
-import com.google.ar.core.Session;
 import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.HitTestResult;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
-import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
-import com.google.ar.sceneform.ux.BaseArFragment;
-import com.google.ar.sceneform.ux.TransformableNode;
-
-import java.util.Vector;
 
 public class ArActivity extends AppCompatActivity {
 
-    ArFragment arFragment;
+    private ArFragment arFragment;
     private ModelRenderable mailboxRenderable;
-    ImageView mailbox;
-    View arrayView[];
-    ViewRenderable name_animal;
-    int selected = 1;
     boolean placed = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,5 +87,13 @@ public class ArActivity extends AppCompatActivity {
         mailbox.setLocalRotation(Quaternion.multiply(Quaternion.axisAngle(new Vector3(0,0,1f), 90f), Quaternion.axisAngle(new Vector3(1f,0,0),30f)));
         mailbox.setParent(anchorNode);
         mailbox.setRenderable(mailboxRenderable);
+        mailbox.setOnTapListener(new Node.OnTapListener() {
+            @Override
+            public void onTap(HitTestResult hitTestResult, MotionEvent motionEvent) {
+                Intent intent= new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            }
+        });
     }
 }
