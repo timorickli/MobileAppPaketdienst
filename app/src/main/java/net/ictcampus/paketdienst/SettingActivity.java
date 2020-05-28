7d139d9bf79de6828038ceb2aff62777dd01c22a
Timo Rickli <timo.rickli@ict-campus.net>
2020-05-28 15:26:31 +0200

    Paket

--- app/src/main/java/net/ictcampus/paketdienst/SettingActivity.java
+++ app/src/main/java/net/ictcampus/paketdienst/SettingActivity.java
@@ -5,14 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
-public class SettingActivity extends AppCompatActivity {
-
+public class SettingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private boolean settingDarkCheck;
+    private boolean settingMusicCheck;
+    private boolean settingMusicEffectsCheck;
+    private String spinnerSelection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        //Spinner selection
+        Spinner spinner= (Spinner) findViewById(R.id.settingSpinner);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.settingMapBack, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //ImageButton with ClickListener
         ImageButton ib = findViewById(R.id.imageButton);
         ib.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +37,42 @@ public class SettingActivity extends AppCompatActivity {
                 overridePendingTransition(0, 0);
             }
         });
+        //Switch Button Listener
+        //Music
+        Switch settingMusic = (Switch) findViewById(R.id.settingMusic);
+        if(settingMusic.isChecked()){
+            settingMusicCheck=true;
+        }
+        else if(!settingMusic.isChecked()){
+            settingMusicCheck=false;
+        }
+        //SoundEffects
+        Switch settingMusicEffects= (Switch) findViewById(R.id.settingMusicEffects);
+        if(settingMusicEffects.isChecked()){
+            settingMusicEffectsCheck=true;
+        }
+        else if(!settingMusicEffects.isChecked()){
+            settingMusicEffectsCheck=false;
+        }
+        //DarkMode
+        Switch settingDark =(Switch) findViewById(R.id.settingDark);
+        if(settingDark.isChecked()){
+            settingDarkCheck=true;
+        }
+        else if(!settingDark.isChecked()){
+            settingDarkCheck=false;
+        }
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        spinnerSelection= parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        spinnerSelection= parent.getItemAtPosition(0).toString();
     }
 }
